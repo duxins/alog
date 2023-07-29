b5b0cdebd9a070aa38681c774dad2b870dd10ef2
Xin Du <duxins@gmail.com>
2023-07-29 22:44:02 +0900

    Remove gpt_4_32k

--- Sources/Models/OpenAIChatModel.swift
+++ Sources/Models/OpenAIChatModel.swift
@@ -11,14 +11,12 @@ enum OpenAIChatModel: String, CaseIterable {
     case gpt_3_5
     case gpt_3_5_16k
     case gpt_4
-    case gpt_4_32k
     
     var displayName: String {
         switch self {
         case .gpt_3_5: return "GPT-3.5-turbo"
         case .gpt_3_5_16k: return "GPT-3.5-turbo-16k"
         case .gpt_4: return "GPT-4"
-        case .gpt_4_32k: return "GPT-4-32k"
         }
     }
     
@@ -27,7 +25,6 @@ enum OpenAIChatModel: String, CaseIterable {
         case .gpt_3_5: return "gpt-3.5-turbo"
         case .gpt_3_5_16k: return "gpt-3.5-turbo-16k"
         case .gpt_4: return "gpt-4"
-        case .gpt_4_32k: return "gpt-4-32k"
         }
     }
 }
