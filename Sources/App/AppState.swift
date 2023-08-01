beec54e45d79614fd88fd877d252aa00eec98824
Xin Du <duxins@gmail.com>
2023-08-01 20:09:24 +0900

    Store premium status in keychain

--- Sources/App/AppState.swift
+++ Sources/App/AppState.swift
@@ -11,12 +11,22 @@ import AVFoundation
 import XLog
 import XLang
 import SwiftUI
+import KeychainAccess
 
 class AppState: ObservableObject {
     static let shared = AppState()
-    private init() {}
+    
+    private init() {
+        if UserDefaults.standard.bool(forKey: PREMIUM_KEY) {
+            isPremium = true
+            UserDefaults.standard.removeObject(forKey: PREMIUM_KEY)
+        } else {
+            isPremium = keychain[string: PREMIUM_KEY] != nil
+        }
+    }
     
     private let l10n = XLang.shared
+    private let keychain = Keychain(service: Bundle.main.bundleIdentifier!)
     
     @Published var language: Language = XLang.shared.currentLang {
         didSet {
@@ -29,8 +39,17 @@ class AppState: ObservableObject {
     @Published var activeSheet: ActiveSheet?
     @Published var activeTab: Int = 0
     
-    @AppStorage("is_premium") var isPremium = false
-
+    private let PREMIUM_KEY = "is_premium"
+    @Published var isPremium: Bool = false {
+        didSet {
+            if isPremium == false {
+                keychain[PREMIUM_KEY] = nil
+            } else {
+                keychain[string: PREMIUM_KEY] = "1"
+            }
+        }
+    }
+    
     func checkMicPermission() {
         let audioSession = AVAudioSession.sharedInstance()
         micPermission = audioSession.recordPermission
