f602bf88b1a767d576224a9e42886f15acbdd83a
Xin Du <duxins@gmail.com>
2023-08-04 10:47:39 +0900

    Add watch target

---
+++ Watch/App/WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchApp.swift
+//  ALogWatch
+//
+//  Created by Xin Du on 2023/08/04.
+//
+
+import SwiftUI
+
+@main
+struct ALogWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
