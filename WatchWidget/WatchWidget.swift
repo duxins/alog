288e1c68454a9b7a8f4591102dad199e278f1e28
Xin Du <duxins@gmail.com>
2023-08-07 13:33:46 +0900

    #3 Add watch Complication

---
+++ WatchWidget/WatchWidget.swift
@@ -0,0 +1,68 @@
+//
+//  WatchWidget.swift
+//  ALogWatchWidget
+//
+//  Created by Xin Du on 2023/08/07.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date())
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date())
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        let entry = SimpleEntry(date: Date.distantFuture)
+        let timeline = Timeline(entries: [entry], policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+}
+
+struct WatchWidgetEntryView : View {
+    var entry: Provider.Entry
+    
+    @Environment(\.widgetFamily) var family
+
+    var body: some View {
+        ZStack {
+            AccessoryWidgetBackground()
+            if family == .accessoryCircular {
+                Image("logo-circular")
+            } else {
+                Image("logo-corner")
+            }
+        }
+    }
+}
+
+@main
+struct WatchWidget: Widget {
+    let kind: String = "XXX"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            WatchWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("ALog")
+        .description("")
+        .supportedFamilies([.accessoryCircular, .accessoryCorner])
+    }
+}
+
+struct WatchWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        WatchWidgetEntryView(entry: SimpleEntry(date: Date()))
+            .previewContext(WidgetPreviewContext(family: .accessoryCorner))
+    }
+}
