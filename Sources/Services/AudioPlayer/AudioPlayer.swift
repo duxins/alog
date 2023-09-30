35b86d9a1329519d1658b4cd944757a6c4ac5a3f
Xin Du <duxins@gmail.com>
2023-10-01 00:16:43 +0900

    Refactor `load(url:)` method in AudioPlayer

--- Sources/Services/AudioPlayer/AudioPlayer.swift
+++ Sources/Services/AudioPlayer/AudioPlayer.swift
@@ -6,6 +6,7 @@
 //
 
 import AVFoundation
+import XLog
 
 class AudioPlayer: NSObject, ObservableObject, AVAudioPlayerDelegate {
     
@@ -17,25 +18,25 @@ class AudioPlayer: NSObject, ObservableObject, AVAudioPlayerDelegate {
     
     convenience init(_ url: URL){
         self.init()
-        load(url: url)
+        try? load(url: url)
     }
 
-    func load(url: URL) {
+    func load(url: URL) throws {
         audioPlayer?.stop()
-        do {
-            audioPlayer = try AVAudioPlayer(contentsOf: url)
-            audioPlayer?.delegate = self
-            audioPlayer?.prepareToPlay()
-        } catch {
-            print("Error loading audio player: \(error)")
-        }
+        audioPlayer = try AVAudioPlayer(contentsOf: url)
+        audioPlayer?.delegate = self
+        audioPlayer?.prepareToPlay()
     }
     
     func play(file: String) {
         currentFile = file
         let url = FileHelper.fullAudioURL(for: file)
-        load(url: url)
-        play()
+        do {
+            try load(url: url)
+            play()
+        } catch {
+            XLog.error(error, source: "Player")
+        }
     }
     
     func play() {
