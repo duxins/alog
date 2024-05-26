f38ca9a2b925f6281395c680622f88cc3d9f15fb
Xin Du <duxins@gmail.com>
2024-05-27 08:53:36 +0900

    #43 Add support for GPT-4o model

--- Sources/Models/OpenAIChatModel.swift
+++ Sources/Models/OpenAIChatModel.swift
@@ -11,12 +11,14 @@ enum OpenAIChatModel: String, CaseIterable {
     case gpt_3_5
     case gpt_3_5_16k
     case gpt_4
+    case gpt_4o
     
     var displayName: String {
         switch self {
         case .gpt_3_5: return "GPT-3.5-turbo"
         case .gpt_3_5_16k: return "GPT-3.5-turbo-16k"
         case .gpt_4: return "GPT-4"
+        case .gpt_4o: return "GPT-4o"
         }
     }
     
@@ -25,6 +27,7 @@ enum OpenAIChatModel: String, CaseIterable {
         case .gpt_3_5: return "gpt-3.5-turbo"
         case .gpt_3_5_16k: return "gpt-3.5-turbo-16k"
         case .gpt_4: return "gpt-4"
+        case .gpt_4o: return "gpt-4o"
         }
     }
 }
